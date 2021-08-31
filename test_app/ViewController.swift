f59588d3f37a817bf9a7df93837052ad1bc85bd3
maksgurkalov <67781150+maksgurkalov@users.noreply.github.com>
2021-08-31 12:15:57 +0300

    Initial Commit

---
+++ test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_app
+//
+//  Created by maks on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
